ced6a6bda92995cc02efeeba49d0aa681a2efe86
ttothewiggy <76453825+ttothewiggy@users.noreply.github.com>
2022-03-21 11:12:11 +1300

    Added dropdown menu

--- SCP Subject Files/JS/app.js
+++ SCP Subject Files/JS/app.js
@@ -1,7 +1,30 @@
+// variables for menu on mobile
+
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
 
+// toggle function to open and close menubar when screen is less than 800px
+
 navToggle.addEventListener('click', function () {
     links.classList.toggle('show-links');
 });
+
+// function for cataogue dropdown menu
+
+// function dropdown() {
+//     document.getElementById('myDropdown').classList.toggle('show');
+// }
+
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')){
+//         var dropdowns = document.getElementsByClassName('dropdown-content');
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++){
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')){
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
